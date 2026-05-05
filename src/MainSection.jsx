09e3c83dbb1a23827db304dec659cce8e0ff637e
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-04 17:30:01 -0700

    Conditional rendering for ingredients on hand

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -31,13 +31,13 @@ function MainSection() {
             {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{mapped}</ul>
-                <div className="get-recipe-container">
+                {ingredients.length >= 4 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
             
         </main>
