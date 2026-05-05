a7698f74929d41b4db4b39e0ca8fd9e8cf0779a3
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-04 17:23:18 -0700

    Added ingredients on hand section

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -11,7 +11,9 @@ function MainSection() {
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        setIngredients( prev => [...prev, newIngredient]);
+        if(newIngredient.length != 0){
+            setIngredients( prev => [...prev, newIngredient]);
+        }
         
     }
 
@@ -27,9 +29,17 @@ function MainSection() {
                 <button>Add ingredient</button>
             </form>
 
-            <ul>
-                {mapped}
-            </ul>
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{mapped}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
         </main>
     )
 }
